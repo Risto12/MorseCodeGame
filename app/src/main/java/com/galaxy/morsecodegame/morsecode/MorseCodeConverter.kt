d73a0365569d3a64f72933af205bb80ef1d4f1e4
Risto12 <risto.vesanto1112@gmail.com>
2023-03-28 19:11:33 +0300

    chore: remove deprecated comment

--- app/src/main/java/com/galaxy/morsecodegame/morsecode/MorseCodeConverter.kt
+++ app/src/main/java/com/galaxy/morsecodegame/morsecode/MorseCodeConverter.kt
@@ -62,7 +62,7 @@ object MorseCodeConverter {
         return if (separatedWords.size > 1) {
             wordsToMorseCode(separatedWords)
         } else {
-            wordToMorseCode(separatedWords.first()) // TODO there is still illegal character in here when using hard difficult level
+            wordToMorseCode(separatedWords.first())
         }
     }
 
